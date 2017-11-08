e3fc50cab539751f284e74ae8f8c84f73513b509
KWall1202 <kjwall97@yahoo.com>
2017-11-08 11:17:45 -0500

    Updated Assignment06P2

--- Assignments/assignment06P2/src/assignment06P2/Array2Dexamples.java
+++ Assignments/assignment06P2/src/assignment06P2/Array2Dexamples.java
@@ -124,4 +124,89 @@ public class Array2Dexamples {
 		}
 		return returnVal;
 	}
+	
+	public static void main(String[] args) {
+		int[][] test = {{1,7,2,4}, {3,5}, {4,0,2,1,4}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {{1,7,2,4}, {3,5,6,1}, {5,5,1,1}, {4,0,2,1,}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {{}, {}, {}, {}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {{1,7,2,4}, {}, {4,0,2,1,4}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {{}, {}, {4,0,2,1,4}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = new int[][] {{0}, {}, {0,0}};
+		System.out.println(Arrays.toString(rowSums(test)));		
+		System.out.println(Arrays.toString(columnSums(test)));
+		System.out.println(Arrays.toString(rowAverages(test)));
+		System.out.println(Arrays.toString(columnAverages(test)));
+		test = null;
+		try {
+			diagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got diagonal exception 1 OK for null");
+		}
+		try {
+			antiDiagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got antidiagonal exception 2 OK for null");
+		}
+		test = new int[][] {{1,7,2,4}, null, {4,0,2,1,4}};
+		try {
+			diagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got diagonal exception 3 OK for null");
+		}
+		try {
+			antiDiagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got antidiagonal exception 4 OK for null");
+		}
+		test = new int[][] {{1,7,2}, {1,7,2}, {4,0,2,1,4}};
+		try {
+			diagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got diagonal exception 5 OK for null");
+		}
+		try {
+			antiDiagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got antidiagonal exception 6 OK for null");
+		}
+		test = new int[][] {{1,7,2}, {1,7}, {4,0,2}};
+		try {
+			diagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got diagonal exception 7 OK for null");
+		}
+		try {
+			antiDiagonalSum(test);
+		} catch(IllegalArgumentException e) {
+			System.out.println("got antidiagonal exception 8 OK for null");
+		}
+		test = new int[][] {{1,7,2,4}, {3,5,6,1}, {5,5,1,1}, {4,0,2,1,}};
+		System.out.println(antiDiagonalSum(test));
+		System.out.println(diagonalSum(test));
+	}
 }
